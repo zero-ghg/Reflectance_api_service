be590efdfaa721151e3ca0a22328073820c9534e
zero-ghg <3153911035@qq.com>
2026-05-25 10:24:56 +0800

    update

--- apps/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from apps.users.view.users import UserInfoLoginView, UserListView
-urlpatterns = [
-    path('login/', UserInfoLoginView.as_view()),
-    path('user/', UserListView.as_view())
-]
