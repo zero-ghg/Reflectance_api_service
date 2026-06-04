40af73e8f1b22174508b73550da3182f78798dff
zero-ghg <3153911035@qq.com>
2026-06-04 13:39:08 +0800

    update

--- apps/reflectance/celery_beat/beat_schedule.py
+++ apps/reflectance/celery_beat/beat_schedule.py
@@ -5,12 +5,12 @@ CELERY_BEAT_SCHEDULE = {
         "task": "reflectance.celery_beat.tasks.run_reflectance_schedule_task",
         "schedule": crontab(minute="1-59/6"),
     },
-    "precipitation-1h-every-6-minutes": {
-        "task": "reflectance.celery_beat.tasks.run_precipitation_1h_schedule_task",
-        "schedule": crontab(minute="1-59/6"),
-    },
-    "precipitation-3h-hourly": {
-        "task": "reflectance.celery_beat.tasks.run_precipitation_3h_schedule_task",
-        "schedule": crontab(minute="1"),
-    },
+    # "precipitation-1h-every-6-minutes": {
+    #     "task": "reflectance.celery_beat.tasks.run_precipitation_1h_schedule_task",
+    #     "schedule": crontab(minute="1-59/6"),
+    # },
+    # "precipitation-3h-hourly": {
+    #     "task": "reflectance.celery_beat.tasks.run_precipitation_3h_schedule_task",
+    #     "schedule": crontab(minute="1"),
+    # },
 }
