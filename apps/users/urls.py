4772f0f02e3c745f3dcde2bd5380beb672135395
zero-ghg <3153911035@qq.com>
2026-06-11 09:23:45 +0800

    验证码

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from apps.users.view.users import UserInfoLoginView, UserListView,UpdateUserView,DeleteUserView,CreateUserInfoView,UserTokenRefreshView
+from apps.users.view.users import UserInfoLoginView, UserListView,UpdateUserView,DeleteUserView,CreateUserInfoView,UserTokenRefreshView,CaptchaGenerateView
 urlpatterns = [
     path('login/', UserInfoLoginView.as_view()),
     path('refresh/', UserTokenRefreshView.as_view()),
     path('create/', CreateUserInfoView.as_view()),
     path('list/', UserListView.as_view()),
     path('update/', UpdateUserView.as_view()),
-    path('delete/', DeleteUserView.as_view())
+    path('delete/', DeleteUserView.as_view()),
+    path('captcha/generate/', CaptchaGenerateView.as_view()),
 ]
